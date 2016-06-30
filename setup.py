5f03c0fddc31cff1a27c2203c7543c5c6acf2352
aLittleTrain <alittletrain@gmail.com>
2016-06-30 16:47:04 +0800

    make it

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='Shua',
+    version='1.0',
+    packages=[''],
+    url='https://github.com/aLittleTrain/Shua',
+    license='LGPL',
+    author='aLittleTrain',
+    author_email='alittletrain@gmail.com',
+    description='A multi-changing data and character output classes'
+)
